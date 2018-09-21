flarum/flarum-core

commit 67d3e1c15251c3c9aaa2416f5ed1bf0953f57d45

Change tab title after renaming discussion (#1543)
* Change tab title after renaming discussion

* Uncommit dist

* Uncommit package-lock.json

* Fix for VDOM

* Move setTitle into DiscussionPage#config

js/src/forum/components/DiscussionPage.js
@@ -115,6 +115,12 @@ export default class DiscussionPage extends Page {
     );
   }
 
+  config() {
+    if (this.discussion) {
+      app.setTitle(this.discussion.title());
+    }
+  }
+
   /**
    * Clear and reload the discussion.
    */
@@ -160,7 +166,6 @@ export default class DiscussionPage extends Page {
     this.discussion = discussion;
 
     app.history.push('discussion', discussion.title());
-    app.setTitle(discussion.title());
     app.setTitleCount(0);
 
     // When the API responds with a discussion, it will also include a number of

commit 04b8b82ed79fb7525aa14a10ba4d13862ca9c625

Fix regression introduced by #1543
The Page component's config method wasn't being called, therefore the
bodyClass would not be applied.

js/src/forum/components/DiscussionPage.js
@@ -115,7 +115,9 @@ export default class DiscussionPage extends Page {
     );
   }
 
-  config() {
+  config(...args) {
+    super.config(...args);
+
     if (this.discussion) {
       app.setTitle(this.discussion.title());
     }