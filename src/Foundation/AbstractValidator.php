51f9dec1ca133fdb4f6d37aa2f2185833f7b4b3d
Davide Iadeluca <146922689+DavideIadeluca@users.noreply.github.com>
2024-11-20 09:51:17 +0100

    [1.x] Implement Support for Translatable Validation Attribute Errors (#4070)
    * chore: add validation translations
    
    * feat: implement ability to translate validation attributes
    
    * chore: change translation key
    
    * style: formatting
    
    * perf: cache `getAttributeNames`
    
    * perf: cache `getAttributeNames`
    
    * chore
    
    * style: formatting

--- src/Foundation/AbstractValidator.php
+++ src/Foundation/AbstractValidator.php
@@ -9,6 +9,7 @@
 
 namespace Flarum\Foundation;
 
+use Illuminate\Contracts\Cache\Store as Cache;
 use Illuminate\Support\Arr;
 use Illuminate\Validation\Factory;
 use Illuminate\Validation\ValidationException;
@@ -16,6 +17,13 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 abstract class AbstractValidator
 {
+    use ExtensionIdTrait;
+
+    /**
+     * @var string
+     */
+    public static $CORE_VALIDATION_CACHE_KEY = 'core.validation.extension_id_class_names';
+
     /**
      * @var array
      */
@@ -82,6 +90,30 @@ abstract class AbstractValidator
     }
 
     /**
+     * @return array
+     */
+    protected function getAttributeNames()
+    {
+        $cache = resolve(Cache::class);
+
+        if ($cache->get(self::$CORE_VALIDATION_CACHE_KEY) !== null) {
+            return $cache->get(self::$CORE_VALIDATION_CACHE_KEY);
+        }
+
+        $extId = $this->getClassExtensionId();
+        $attributeNames = [];
+
+        foreach (array_keys($this->rules) as $attribute) {
+            $key = $extId ? "$extId.validation.attributes.$attribute" : "validation.attributes.$attribute";
+            $attributeNames[$attribute] = $this->translator->trans($key);
+        }
+
+        $cache->forever(self::$CORE_VALIDATION_CACHE_KEY, $attributeNames);
+
+        return $attributeNames;
+    }
+
+    /**
      * Make a new validator instance for this model.
      *
      * @param array $attributes
@@ -92,6 +124,7 @@ abstract class AbstractValidator
         $rules = Arr::only($this->getRules(), array_keys($attributes));
 
         $validator = $this->validator->make($attributes, $rules, $this->getMessages());
+        $validator->setAttributeNames($this->getAttributeNames());
 
         foreach ($this->configuration as $callable) {
             $callable($this, $validator);
