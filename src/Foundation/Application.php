ebbec7283173a8b1ea6894f6b22662cc16bb724c
Sami Mazouz <sychocouldy@gmail.com>
2025-04-25 10:19:54 +0100

    chore: prepare 2.0.0-beta.3

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -25,7 +25,7 @@ class Application extends IlluminateContainer implements LaravelApplication
      *
      * @var string
      */
-    public const VERSION = '2.0.0-beta.2';
+    public const VERSION = '2.0.0-beta.3';
 
     protected bool $booted = false;
 
